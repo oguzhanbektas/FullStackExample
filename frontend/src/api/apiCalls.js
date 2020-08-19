11e7e9040cb0ccde3326c470314115c5b7a2e57d
oguzhanbektas <ozibektas@gmail.com>
2020-08-19 11:49:34 +0300

    ws Validationmessages for language frontend post  header accept-language for language

--- frontend/src/api/apiCalls.js
+++ frontend/src/api/apiCalls.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-export const signup = body => {
-  return axios.post("/api/1.0/users", body);
+export const signup = (body) => {
+  return axios.post("/api/1.0/users", body, {
+    headers: { "accept-language": "tr" },
+  });
 };
--- ws/src/main/java/com/hoaxify/ws/user/User.java
+++ ws/src/main/java/com/hoaxify/ws/user/User.java
@@ -14,22 +14,22 @@ import lombok.Data;
 @Entity
 public class User {
 
-	@Id
-	@GeneratedValue
-	private long id;
+    @Id
+    @GeneratedValue
+    private long id;
 
-	@NotNull
-	@Size(min = 4, max = 255)
-	@UniqueUsername
-	private String username;
+    @NotNull(message = "{hoaxify.constraint.username.NotNull.message}")
+    @Size(min = 4, max = 255)
+    @UniqueUsername
+    private String username;
 
-	@NotNull
-	@Size(min = 4, max = 255)
-	private String displayName;
+    @NotNull
+    @Size(min = 4, max = 255)
+    private String displayName;
 
-	@NotNull
-	@Size(min = 8, max = 255)
-	@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$")
-	private String password;
+    @NotNull
+    @Size(min = 8, max = 255)
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$", message = "{hoaxify.constraint.password.Pattern.message}")
+    private String password;
 
 }
