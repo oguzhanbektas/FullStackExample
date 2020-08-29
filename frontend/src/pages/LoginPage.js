51b108da935a87070185cf3b884f83b41eec813d
Oğuzhan Bektaş <ozibektas@gmail.com>
2020-08-29 13:28:30 +0300

    login page is created.

---
+++ frontend/src/pages/LoginPage.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import Input from "../components/Input";
+
+export default class LoginPage extends Component {
+  state = {
+    username: null,
+    password: null,
+  };
+  onChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+  render() {
+    return (
+      <div className="container">
+        <form>
+          <h1 className="text-center"> Login </h1>
+          <Input label="Username" name="username" onChange={this.onChange} />
+          <Input
+            label="Password"
+            name="password"
+            type="password"
+            onChange={this.onChange}
+          />
+          <div className="text-center">
+            <button className="btn btn-primary"> Login </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
