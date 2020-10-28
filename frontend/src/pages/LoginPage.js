61cc0681c08f11a3c550d4d1f0daa55cb6b20b35
Oğuzhan Bektaş <ozibektas@gmail.com>
2020-10-28 07:42:45 +0300

    Frontend -> Basic auth structure created.

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,25 +1,42 @@
 import React, { Component } from "react";
 import Input from "../components/Input";
 import { withTranslation } from "react-i18next";
+import { login } from "../api/apiCalls";
 
 class LoginPage extends Component {
   state = {
     username: null,
     password: null,
   };
+
   onChange = (event) => {
     const { name, value } = event.target;
     this.setState({
       [name]: value,
     });
   };
+
+  onClickLogin = (event) => {
+    event.preventDefault();
+    const { username, password } = this.state;
+    const creds = {
+      username,
+      password,
+    };
+    login(creds);
+  };
+
   render() {
     const { t } = this.props;
     return (
       <div className="container">
         <form>
           <h1 className="text-center"> {t("Login")} </h1>
-          <Input label={t("Username")} name="username" onChange={this.onChange} />
+          <Input
+            label={t("Username")}
+            name="username"
+            onChange={this.onChange}
+          />
           <Input
             label={t("Password")}
             name="password"
@@ -27,7 +44,7 @@ class LoginPage extends Component {
             onChange={this.onChange}
           />
           <div className="text-center">
-            <button className="btn btn-primary"> {t("Login")} </button>
+            <button className="btn btn-primary" onClick={this.onClickLogin}> {t("Login")}  </button>
           </div>
         </form>
       </div>
