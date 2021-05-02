b9c825ba8288137a4e8f4db0a45ea9e30a95a82e
Oğuzhan BEKTAŞ <ozibektas@gmail.com>
2021-05-02 13:01:09 +0300

    Authentication Failure on Client (React)

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -7,27 +7,42 @@ class LoginPage extends Component {
   state = {
     username: null,
     password: null,
+    error: null,
   };
 
   onChange = (event) => {
     const { name, value } = event.target;
     this.setState({
       [name]: value,
+      error: null,
     });
   };
 
-  onClickLogin = (event) => {
+  onClickLogin = async (event) => {
     event.preventDefault();
     const { username, password } = this.state;
     const creds = {
       username,
       password,
     };
-    login(creds);
+    this.setState({
+      error: null,
+    });
+    try {
+      await login(creds);
+    } catch (apiError) {
+      this.setState({
+        error: apiError.response.data.message,
+      });
+    }
   };
 
   render() {
     const { t } = this.props;
+    const { username, password, error } = this.state;
+
+    const buttonEnabled = username && password;
+
     return (
       <div className="container">
         <form>
@@ -43,8 +58,17 @@ class LoginPage extends Component {
             type="password"
             onChange={this.onChange}
           />
+          {error && (
+            <div className="alert alert-danger">{error}</div>
+          )}
           <div className="text-center">
-            <button className="btn btn-primary" onClick={this.onClickLogin}> {t("Login")}  </button>
+            <button
+              className="btn btn-primary"
+              onClick={this.onClickLogin}
+              disabled={!buttonEnabled}
+            >
+              {t("Login")}
+            </button>
           </div>
         </form>
       </div>
