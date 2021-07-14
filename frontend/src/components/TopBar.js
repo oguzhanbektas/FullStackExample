306d4f81a459152d436dd6c9d123cac0104b7e2d
Oğuzhan BEKTAŞ <ozibektas@gmail.com>
2021-07-14 21:01:13 +0300

    topbar changes

--- frontend/src/components/TopBar.js
+++ frontend/src/components/TopBar.js
@@ -3,26 +3,46 @@ import { withTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 
 class TopBar extends Component {
+
+  state = { isLoggedIn: true, username: 'user1' };
+
   render() {
     const { t } = this.props;
+    const { isLoggedIn, username } = this.state;
+
+    let links = (
+      <ul className="navbar-nav ml-auto">
+        <li>
+          <Link className="nav-link" to="/login">
+            {t("Login")}
+          </Link>
+        </li>
+        <li>
+          <Link className="nav-link" to="/signup">
+            {t("Sign Up")}
+          </Link>
+        </li>
+      </ul>
+    );
+    if (isLoggedIn) {
+      links = (<ul className="navbar-nav ml-auto">
+        <li>
+          <Link className="nav-link" to={`/user/${username}`}>
+            {username}
+          </Link>
+        </li>
+        <li className="nav-link">
+          {t('Logout')}
+        </li>
+      </ul>);
+    }
     return (
       <div className="shadow-sm bg-light mb-2">
         <nav className="navbar navbar-light container navbar-expand">
           <Link className="navbar-brand" to="/">
             Hoaxify
           </Link>
-          <ul className="navbar-nav ml-auto">
-            <li>
-              <Link className="nav-link" to="/login">
-                {t("Login")}
-              </Link>
-            </li>
-            <li>
-              <Link className="nav-link" to="/signup">
-                {t("Sign Up")}
-              </Link>
-            </li>
-          </ul>
+          {links}
         </nav>
       </div>
     );
--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -7,22 +7,24 @@ i18n.use(initReactI18next).init({
       translations: {
         "Signup": "Sign up",
         "Password mismatch": "Password mismatch",
-        "Username":"Username",
+        "Username": "Username",
         "Displayname": "Displayname",
-        "Password":"Password",
-        "Password Repeat":"Password Repeat",
-        "Login":"Login"
+        "Password": "Password",
+        "Password Repeat": "Password Repeat",
+        "Login": "Login",
+        "Logout": "Logout"
       },
     },
     tr: {
       translations: {
         "Signup": "Kayıt Ol",
         "Password mismatch": "Şifreler aynı olmalı",
-        "Username":"Kullanıcı Adı",
+        "Username": "Kullanıcı Adı",
         "Displayname": "Takma Ad",
-        "Password":"Şifre",
-        "Password Repeat":"Şifreyi Tekrarla",
-        "Login":"Sisteme Gir"
+        "Password": "Şifre",
+        "Password Repeat": "Şifreyi Tekrarla",
+        "Login": "Sisteme Gir",
+        "Logout": "Çıkış Yap"
       },
     },
   },
